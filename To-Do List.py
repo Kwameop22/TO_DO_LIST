f36aa2e045b097e5f85bf9494e37c91e07dff6c8
Kwame Opoku Agyeman <Kwameagyeman22@yahoo.com>
2025-12-29 22:54:03 +0000

    Update To-Do List.py

--- To-Do List.py
+++ To-Do List.py
@@ -2,10 +2,11 @@ print("Welcome to your To-Do List!")
 tasks = []
 
 while True:
-    print("\n\033[4m MENU \033[0m")  # Underlined Menu
-    print("1. Add task")
-    print("2. View task")
-    print("3. Quit")
+    print("""
+   1. Add task
+   2. View task
+   3. Quit
+   """)
 
     choice = input("Choice: ")
 
@@ -14,12 +15,13 @@ while True:
             task = input("Enter a new task (or type 'quit' to stop): ")
             if task.lower() == "quit":
                 break
-
             tasks.append(task)
             print("Task added!")
 
+
     elif choice == "2":
-        print("\nYour tasks:")
+        print(" ")
+        print("Your tasks:")
         for i, t in enumerate(tasks, 1):
             print(f"{i}. {t}")
 
@@ -28,3 +30,6 @@ while True:
         break
     else:
         print("Invalid choice! Try again.")
+
+
+
