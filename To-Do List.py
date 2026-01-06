9be2f5d28e6d9659e6bc4169f02e84645bacc49d
Kwame Opoku Agyeman <Kwameop225@outlook.com>
2026-01-06 07:51:42 +0000

    Update To-Do List.py

--- To-Do List.py
+++ To-Do List.py
@@ -1,20 +1,21 @@
-print("Welcome to your To-Do List!")
-tasks = []
+print("Welcome to your To-Do List!") # This creates a title which says "Welcome to your To-Do List!"
+tasks = []# This stores the task you add
 
 while True:
     print("""
    1. Add task
    2. View task
    3. Quit
-   """)
+   """) # This is a menu which shows 3 options you want to choose from(Add task,View task,Quit)
 
-    choice = input("Choice: ")
+    choice = input("Choice: ") # Enables you to choose an option
 
-    if choice == "1":
-        while True:
-            task = input("Enter a new task (or type 'quit' to stop): ")
-            if task.lower() == "quit":
+    if choice == "1": # This shows what happens when you use option 1
+        while True: # This makes the action in the code in choice 1 repeat multiple times / loop unless you type "quit"
+            task = input("Enter a new task (or type 'quit' to stop): ") # This enables you to add a new task
+            if task.lower() == "quit": 
                 break
+            # This code stops the loop when you type quit
             tasks.append(task)
             print("Task added!")
 
